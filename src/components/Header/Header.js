e0ab53aa28086fed64159fc42f8c8aa22b50babf
Mayank Nasa <nasamayank1@gmaill.com>
2023-12-02 14:53:32 +0530

    fixing built error

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import "./header.css";
 import React from "react";
+import Marquee from "react-fast-marquee";
+
 
 function Header() {
   return (
     <nav>
       <div className="header-item-container">
-      <div className="header-marquee"><marquee>&#127918;&nbsp;<span>Level Up Gaming is now available at your service!</span>&nbsp;&#127918;</marquee></div>
+      <div className="header-marquee"><Marquee>&#127918;&nbsp;<span>Level Up Gaming is now available at your service!</span>&nbsp;&#127918;</Marquee></div>
       <div className="header-items">
         <ul class="menu">
           <li><a href="#main">Home</a></li>
