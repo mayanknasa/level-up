bdb828698eec21ed248ed5c90a57127578555930
Mayank Nasa <nasamayank1@gmaill.com>
2024-01-27 10:49:06 +0530

    editing portfolio linl

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='footer-container'>website created with&nbsp;<span className='love-icon'>&#10084; </span>&nbsp;by&nbsp;<a href='https://mayanknasa.netlify.app/' target='__blank'> Mayank Nasa</a></footer>
+    <footer className='footer-container'>website created with&nbsp;<span className='love-icon'>&#10084; </span>&nbsp;by&nbsp;<a href='https://www.mayanknasa.com' target='__blank'> Mayank Nasa</a></footer>
   )
 }
 
